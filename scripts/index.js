c8c479f0e8cc204734f52e251f1cbfb033dc298d
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-07-02 20:55:55 -0600

    feat(new projects): added a couple new projects to the index.js to test that the card switcher was built correctly

--- scripts/index.js
+++ scripts/index.js
@@ -8,5 +8,20 @@ const projects = [
     name: "Kirk LVNDR",
     link: "https://kirklvndr.com/",
     desc: "A landing page I created for one of my good friend's musician identity"
+  },
+  {
+    name: "Resume Page",
+    link: "https://graydonwasil.com/",
+    desc: "This page you are on right now. Enjoy the infinite loop"
+  },
+  {
+    name: "Portfolio Project",
+    link: "https://arrangedgodly.com/portfolio-project/",
+    desc: "the original iteration of my portfolio project"
+  },
+  {
+    name: "Arranged Godly",
+    link: "https://arrangedgodly.com/",
+    desc: "my very first webpage"
   }
 ];
