35c192b6404bf55922bc9aba77e571f401c40520
Arrangedgodly <arrangedgodly@gmail.com>
2022-07-02 19:04:17 -0600

    started fixing project section with JS

--- scripts/projects.js
+++ scripts/projects.js
@@ -13,8 +13,45 @@ function createProject(data) {
 }
 
 const projectList = document.querySelector(".list_flex_project");
+const projectListElement = document.querySelector(".list_project");
+let projectCount = 0;
+const leftArrow = document.querySelector(".icon_left-arrow");
+const rightArrow = document.querySelector(".icon_right-arrow");
+const leftArrowIcon = document.querySelector(".fa-arrow-left");
+const rightArrowIcon = document.querySelector(".fa-arrow-right");
+const startingProject = createProject(projects[0]);
+projectList.append(startingProject);
 
-for (let i = 0; i < projects.length; i++) {
-  const project = createProject(projects[i]);
-  projectList.append(project);
-};
+checkArrows(projectCount);
+
+function checkArrows(num) {
+  if (num === 0) {
+    leftArrowIcon.setAttribute("style", "opacity: 0;");
+  } else if (num > 0) {
+    leftArrowIcon.setAttribute("style", "opacity: 1;");
+  };
+  if (num === projects.length) {
+    rightArrowIcon.setAttribute("style", "opacity: 0;");
+  } else if (num < projects.length) {
+    rightArrowIcon.setAttribute("style", "opacity: 1;");
+  }
+}
+
+function updateProject(num) {
+  let tempProject = createProject(projects[num]);
+  projectList.replaceChildren(tempProject);
+  checkArrows(projectCount);
+}
+
+function clickRightArrow() {
+  projectCount++;
+  updateProject(projectCount);
+}
+
+function clickLeftArrow() {
+  projectCount--;
+  updateProject(projectCount);
+}
+
+leftArrow.addEventListener("click", clickLeftArrow);
+rightArrow.addEventListener("click", clickRightArrow);
