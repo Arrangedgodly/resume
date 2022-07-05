bece8a15a2fd67a0a263c450a80f1bfe4cee8209
Arrangedgodly <arrangedgodly@gmail.com>
2022-07-05 11:27:35 -0600

    readded https connections lol, all links were broken

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const projects = [
   {
     name: "Vault Counter",
-    link: "http://vault.graydonwasil.com/",
+    link: "https://vault.graydonwasil.com/",
     desc: "An application to assist my coworkers in counting the back of house cash with ease of use and complete accuracy"
   },
   {
     name: "Kirk LVNDR",
-    link: "http://kirklvndr.com/",
+    link: "https://kirklvndr.com/",
     desc: "A landing page I created for one of my good friend's musician identity"
   },
   {
     name: "Resume Page",
-    link: "http://graydonwasil.com/",
+    link: "https://graydonwasil.com/",
     desc: "This page you are on right now. Enjoy the infinite loop"
   },
   {
     name: "Portfolio Project",
-    link: "http://arrangedgodly.com/portfolio-project/",
+    link: "https://arrangedgodly.com/portfolio-project/",
     desc: "the original iteration of my portfolio project"
   },
   {
     name: "Arranged Godly",
-    link: "http://arrangedgodly.com/",
+    link: "https://arrangedgodly.com/",
     desc: "my very first webpage"
   }
 ];
